22d8c7c04c8d8910fe0d9570c6cccd14850ae444
Narcil57 <narcil58elite@gmail.com>
2026-03-16 14:54:48 +0100

    missing csv and disabled wass llm

--- cogs/quiz.py
+++ cogs/quiz.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
-from llm import LangChainBot
+#from llm import LangChainBot
 
 class Quiz(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.llm = LangChainBot()
+        #self.llm = LangChainBot()
 
     @commands.Cog.listener()
     async def on_message(self,message):
